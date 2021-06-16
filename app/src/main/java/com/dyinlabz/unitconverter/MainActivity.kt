bde639e25fd65df8b0993888a354b7a250f4e20d
Samuel Osewa <osewaayomide4@gmail.com>
2021-06-16 21:06:31 +0100

    layout for app done

--- app/src/main/java/com/dyinlabz/unitconverter/MainActivity.kt
+++ app/src/main/java/com/dyinlabz/unitconverter/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.dyinlabz.unitconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dyinlabz.unitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnConvert.setOnClickListener {
+            convertUnit()
+        }
     }
+
+    fun convertUnit(){
+        val getValue = binding.etValue.text.toString()
+        val value = getValue.toDouble()
+
+        val unit = binding.rgUnits.checkedRadioButtonId
+
+        val convertedValue = when(unit){
+            R.id.feet_to_metres -> value / 3.281
+            else -> value * 3.281
+        }
+
+        binding.tvResult?.text = convertedValue.toString()
+
+    }
+    
+
 }
